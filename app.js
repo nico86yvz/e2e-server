4c8a799f607cef9ce195adbc347a53125ec24c68
Nikolas Y <nico86yvz@gmail.com>
2015-10-18 18:40:29 -0400

    Initiliazed repo

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+
+var middleware = {
+  requireAuth: function(req, res, next) {
+      console.log('private route hit');
+      next();
+  },
+  logger: function(req, res, next) {
+
+      console.log(new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+      next();
+  }
+};
+
+app.use(middleware.logger);
+
+app.get('/mal', function(req, res){
+    res.send('malmisin oglum!');
+});
+
+app.use(express.static(__dirname + '/public/views'));
+
+app.listen(PORT, function() {
+console.log('express server is running: ' + PORT);
+});
